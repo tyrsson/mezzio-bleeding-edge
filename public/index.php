6178b5099106c9cd6effe0b3842f636d16ec68cc
Joey Smith <jsmith@webinertia.net>
2026-01-18 15:26:40 -0600

    Update coding standard usage. Apply rules Bumps deps. Signed-off-by: Joey Smith <jsmith@webinertia.net>
    Signed-off-by: Joey Smith <jsmith@webinertia.net>

--- public/index.php
+++ public/index.php
@@ -2,10 +2,10 @@
 
 declare(strict_types=1);
 
-/*
- * This file is part of the Mezzio Bleeding Edge Skeleton App.
+/**
+ * This file is part of the Webware Mezzio Bleeding Edge package.
  *
- * Copyright (c) 2025-2026 Joey Smith <jsmith@webinertia.net>
+ * Copyright (c) 2026 Joey Smith <jsmith@webinertia.net>
  * and contributors.
  *
  * For the full copyright and license information, please view the LICENSE
