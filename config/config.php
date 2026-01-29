14317cb0b01ca503fd4a9e0a07625abe8e9125db
Joey Smith <jsmith@webinertia.net>
2026-01-29 02:13:17 -0600

    Mysqli is working at route /mysql/mysqli Signed-off-by: Joey Smith <jsmith@webinertia.net>
    Signed-off-by: Joey Smith <jsmith@webinertia.net>

--- config/config.php
+++ config/config.php
@@ -34,6 +34,7 @@ $aggregator = new ConfigAggregator([
         },
     // Default App module config
     ConfigProvider::class,
+    \Mysql\ConfigProvider::class,
     // Load application config in a pre-defined order in such a way that local settings
     // overwrite global settings. (Loaded as first to last):
     //   - `global.php`
